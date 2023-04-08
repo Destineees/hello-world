f6f570c333f170faecaa3e6f1ea4417f74136602
Destinee Sanders <destineees@gmail.com>
2023-04-08 10:36:23 -0500

    added some conditionals for fun

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ document.write("Hi"+ userName + " Welcome to Seattle Beaches! Time to explore!")
 //disply greeting on site 
 if (userName == "Kassie"){
     alert("Thank you for teaching!");
+}else {
+    alert("Glad you found this page!");
 }
 if(userName == " "){
     prompt("Do you really not have a name?");
