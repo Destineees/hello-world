b8dd9772ce41cf712e5514a9b3f153999d5c53e0
Destinee Sanders <destineees@gmail.com>
2023-04-08 10:33:11 -0500

    making dynamic with JS

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 let userName = prompt("Hello, What is your name?");
-console.log("Hi"+userName + "welcome to Seattle Beaches!");
-document.write("Hi"+ userName + "Welcome to Seattle Beaches!");
+//get user name 
+document.write("Hi"+ userName + " Welcome to Seattle Beaches! Time to explore!");
+//disply greeting on site 
+if (userName == "Kassie"){
+    alert("Thank you for teaching!");
+}
+if(userName == " "){
+    prompt("Do you really not have a name?");
+}
+if(userName == "Destinee"){
+    alert("Hello Beautiful!")
+}
