4ec4237974cc418ba00e60607cb80b30823aa855
Destinee Sanders <destineees@gmail.com>
2023-04-08 10:39:31 -0500

    learning

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 let userName = prompt("Hello, What is your name?");
 //get user name 
-document.write("Hi"+ userName + " Welcome to Seattle Beaches! Time to explore!");
+document.write(" Hi"+ userName + " Welcome to Seattle Beaches! Time to explore!");
 //disply greeting on site 
 if (userName == "Kassie"){
     alert("Thank you for teaching!");
 }else {
     alert("Glad you found this page!");
-}
+} // if it is the teacher say thank you if not thank guests for finding the page 
 if(userName == " "){
     prompt("Do you really not have a name?");
-}
+}// if they enter nothing or a space ask for their name 
 if(userName == "Destinee"){
     alert("Hello Beautiful!")
 }
+//is it me 
