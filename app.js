aa9e9dd18534c0c6c70dee5c3c313e4a89f8b9fa
Destinee Sanders <destineees@gmail.com>
2023-04-10 19:35:20 -0700

    functions

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-let userName = prompt("Hello, What is your name?");
-//get user name 
-document.write(" Hi"+ userName + " Welcome to Seattle Beaches! Time to explore!");
-//disply greeting on site 
-if (userName == "Kassie"){
-    alert("Thank you for teaching!");
-}else {
-    if(userName == ""){
-        prompt("Do you really not have a name?");
-    }// if they enter nothing or a space ask for their name 
-    if(userName == "Destinee"){
-        alert("Hello Beautiful!")
-    }
-    alert("Glad you found this page!");
 
-} 
+function HelloYouser()
+{
+    let userName = prompt("Hello, What is your name?");
+    //get user name 
+    document.write(" Hi"+ userName + " Welcome to Seattle Beaches! Time to explore!");
+    //disply greeting on site 
+    if (userName == "Kassie"){
+        alert("Thank you for teaching!");
+    }
+    else if (userName == "")
+        {
+         prompt("Do you really not have a name?");
+        }// if they enter nothing prompt again for their name 
+    else if(userName == "Destinee"){
+            alert("Hello Beautiful!")
+        }
+    else {
+        alert("Glad you found this page!");
+        }
+}
+function youserBeach()
+{
+    let userBeach = prompt("Do you have a favorite Beach?");
+    document.write( userName + "LOVES" + youserBeach );
+
+}
