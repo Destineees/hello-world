c2ad58bc3c7cb7e5e70c2221b7ed23a87003faf4
Destinee Sanders <destineees@gmail.com>
2023-04-16 14:24:44 -0700

    learning

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let x;
     {
         userName = prompt("Do you really not have a name?")
     } // if they enter nothing prompt again for their name
-    document.write(" Hi"+ userName + " Welcome to Seattle Beaches! Time to explore!");//disply greeting on site 
+    document.write(" Hi, "+ userName + "! Welcome to Seattle Beaches! Time to explore!");//disply greeting on site 
     if (userName == "Kassie")
     {
         alert("Thank you for teaching!");
@@ -32,13 +32,13 @@ function  youserBeach()
     }//check for blank response 
     if (userBeach == 1)
        choiceBeach = "Alki Beach";
-       x = document.getElementById("1").style.display ='block';
+       x = document.getElementById("1");
     if (userBeach == 2)
     choiceBeach = "Discovery Park Beach";
-    x = document.getElementById("2").style.display ='block';
+    x = document.getElementById("2");
     if (userBeach == 3)
       choiceBeach = "Pocket Beach";
-      x = document.getElementById("3").style.display ='block';
+      x = document.getElementById("3");
     //check for beach options
     if (userBeach == 0)
    prompt("Please enter the number of your favorite beach 1:Alki Beach 2:Discovery Park Beach 3:Pocket Beach"); 
@@ -47,11 +47,7 @@ function  youserBeach()
     //check for numbers greater than 3 and below 0
 
     confirm("Your favorite beach is " + choiceBeach);
-    return choiceBeach;
-}
-//call on button click youserBeach();
-function visitBeach()
-{
+
     let userBeachVisit = prompt("How many times do you want to visit "+ choiceBeach +" ? Enter a number between 0-10");//get beach visits
     while(userBeachVisit == "" || userBeachVisit >11)
     {
@@ -65,5 +61,4 @@ function visitBeach()
       
     }// show image number of times entered for beach visit
 
-}
-//vistBeach();//call on button click
+} //call on button click youserBeach();
